2357be50774eb83eef13943d2cb648d5f26ae4ea
Lobzon-Sanct <Lobzon_sancto@proton.me>
2026-04-21 20:24:46 -0300

    frases ajust

--- assets/js/gallery.js
+++ assets/js/gallery.js
@@ -1,11 +1,32 @@
 // Galeria: edite a lista abaixo com seus arquivos em assets/media
 
 const MEDIA = [
-  { type: 'img', file: 'assets/media/exemplo-1.jpg', caption: 'Exemplo 1' },
-  { type: 'img', file: 'assets/media/exemplo-2.jpg', caption: 'Exemplo 2' },
-  { type: 'video', file: 'assets/media/exemplo-video.mp4', caption: 'Exemplo em vídeo' },
-  // { type: 'video', file: 'assets/media/clip.mp4', caption: 'Um clipe' },
-  // { type: 'img', file: 'assets/media/collage.gif', caption: 'Um gif' },
+  { type: 'img', file: 'assets/media/2025-09-27T00-46-46-240.jpg', caption: 'Select Chale 01' },
+  { type: 'img', file: 'assets/media/2025-09-27T00-46-49-996.jpg', caption: 'Select Chale 02' },
+  { type: 'img', file: 'assets/media/2025-09-27T00-47-00-454.jpg', caption: 'Select Chale 03' },
+  { type: 'img', file: 'assets/media/2025-09-27T00-47-03-211.jpg', caption: 'Select Chale 04' },
+  { type: 'img', file: 'assets/media/2025-09-27T00-47-28-796.jpg', caption: 'Select Chale 05' },
+  { type: 'img', file: 'assets/media/2025-09-27T00-47-33-122.jpg', caption: 'Select Chale 06' },
+  { type: 'img', file: 'assets/media/2025-09-27T00-54-23-538.jpg', caption: 'Select Chale 07' },
+  { type: 'img', file: 'assets/media/2025-09-27T00-54-25-958.jpg', caption: 'Select Chale 08' },
+  { type: 'img', file: 'assets/media/2025-09-27T00-54-33-865.jpg', caption: 'Select Chale 09' },
+  { type: 'img', file: 'assets/media/2025-09-27T00-54-37-993.jpg', caption: 'Select Chale 10' },
+  { type: 'img', file: 'assets/media/2025-09-27T00-55-02-291.jpg', caption: 'Select Chale 11' },
+  { type: 'img', file: 'assets/media/2025-09-27T00-55-12-151.jpg', caption: 'Select Chale 12' },
+  { type: 'img', file: 'assets/media/agarra podio.jpg', caption: 'Agarra Podio' },
+  { type: 'img', file: 'assets/media/bj podio.jpg', caption: 'Bj Podio' },
+  { type: 'img', file: 'assets/media/chamego podio.jpg', caption: 'Chamego Podio' },
+  { type: 'img', file: 'assets/media/chamego01.jpg', caption: 'Chamego 01' },
+  { type: 'img', file: 'assets/media/chamego02.jpg', caption: 'Chamego 02' },
+  { type: 'img', file: 'assets/media/chamego03.jpg', caption: 'Chamego 03' },
+  { type: 'img', file: 'assets/media/croco podio.jpg', caption: 'Croco Podio' },
+  { type: 'img', file: 'assets/media/oiv1.jpg', caption: 'Oiv 1' },
+  { type: 'img', file: 'assets/media/oiv2.jpg', caption: 'Oiv 2' },
+  { type: 'img', file: 'assets/media/pocoto podio.jpg', caption: 'Pocoto Podio' },
+  { type: 'img', file: 'assets/media/podio sim.jpg', caption: 'Podio Sim' },
+  { type: 'img', file: 'assets/media/podio x.jpg', caption: 'Podio X' },
+  { type: 'img', file: 'assets/media/soquinho podio.jpg', caption: 'Soquinho Podio' },
+  { type: 'img', file: 'assets/media/vemk podio.jpg', caption: 'Vemk Podio' }
 ];
 
 function el(tag, cls){
